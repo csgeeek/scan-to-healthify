30cde0ffb94ecb1985b35f50fcae44868a405665
csg33k <balajiyaswanth.v@gmail.com>
2022-05-31 01:09:04 +0530

    added projectid to firebase-config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
