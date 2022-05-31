cf7759b5655fcd355cd42ff757283c96e49f5eff
csg33k <balajiyaswanth.v@gmail.com>
2022-05-31 14:21:04 +0530

    added file upload functionality fastapi

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 
 import easyocr
@@ -16,11 +16,20 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile):
+    print(file.filename)
+    file_location = f"files/{file.filename}"
+    print(file_location)
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
 
-@app.get("/images/{image_id}")
-def read_image(image_id: str):
+@app.post("/upload")
+async def read_image(file: UploadFile):
+    print(file.file)
     reader = easyocr.Reader(['en'])
-    result = reader.readtext(image_id)
+    result = reader.readtext()
     fstr = ''
     for tup in result:
         fstr += tup[1]
