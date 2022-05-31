2d730c395e293b89483146a81692edd65021e261
csg33k <balajiyaswanth.v@gmail.com>
2022-05-31 12:18:57 +0530

    saving fastapi work

--- script.py
+++ script.py
@@ -1,6 +1,29 @@
+from typing import Union
+from fastapi import FastAPI
+
+
 import easyocr
 
-reader = easyocr.Reader(['en'])
-result = reader.readtext('test4.jpg')
+app = FastAPI()
 
-print(result)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/images/{image_id}")
+def read_image(image_id: str):
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(image_id)
+    fstr = ''
+    for tup in result:
+        fstr += tup[1]
+    print(fstr)
+    print(type(fstr))
+    return {fstr}
